3e6af22a985d371186afe64d48320f9a7ed97759
Jordan Albers <albersjordan13@gmail.com>
2020-02-29 19:23:33 -0700

    added and set up connection.js file to connect to the burgers_db, and set up orm.js to get all data, insert data, and update data from the database

---
+++ db/config/orm.js
@@ -0,0 +1,30 @@
+const connection = require('./connection.js');
+
+const orm = {
+
+    selectAll: function(tableInput,cb) {  
+        connection.query("SELECT * FROM ??", [tableInput], function(err, result) {
+            if(err) {throw err};
+
+            cb(result);
+        });
+    },
+    insertOne: function(tablename, col1, col2, val1, val2,cb) {
+        const insert = "INSERT INTO ?? (??,??) VALUES (?,?)";
+        connection.query(insert, [tablename, col1,col2, val1,val2],
+        function (err, result){
+            if (err){throw err};
+
+            cb(result);
+        });
+    },
+    updateOne: function(){
+        const update = "UPDATE ?? SET ?? = ? WHERE ??=?";
+        connection.query(updates, {tablename, col2, col2value, col1, col1value}, function(err, result){
+            if (err) {throw err};
+            cb(result);
+        })
+    }
+};
+
+module.exports = orm;
